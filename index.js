a396ee4c4524dbc38a91a64d83ef6c4df6a106d9
Guillermo-Reyes1 <reyesguillermo2305@gmail.com>
2026-02-23 14:10:57 -0500

    right and left button functions on click

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// left button (back)
-
-// right button (right)
-
-let pokeID = 1
-
 fetch = "https://pokeapi.co/api/v2/pokemon/1"
 
 const infoBtn = document.getElementById("info-btn");
@@ -18,3 +12,24 @@ movesBtn.addEventListener("click", () => {
   movesBtn.classList.add("active");
   infoBtn.classList.remove("active");
 });
+
+let pokeID = 1;
+const rightBtn = document.getElementById("right-btn");
+const leftBtn = document.getElementById("left-btn");
+
+function updatePokemon() {
+    console.log("Current Pokemon ID: ", pokeID)
+    // chage displayed information
+}
+
+function changePokemon(val) {
+    pokeID += val;
+
+    if (pokeID < 1) pokeID = 1;
+    if (pokeID > 1025) pokeID = 1025;
+    updatePokemon()
+}
+
+rightBtn.addEventListener("click", () => changePokemon(+1))
+
+leftBtn.addEventListener("click", () => changePokemon(-1))
