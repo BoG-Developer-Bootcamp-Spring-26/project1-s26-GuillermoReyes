dcdff6559870b4d4ebc6cc2bd1102a4848c3cf35
Guillermo-Reyes1 <reyesguillermo2305@gmail.com>
2026-02-23 13:36:11 -0500

    Created JS File and active info/moves panel

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
 let pokeID = 1
 
 fetch = "https://pokeapi.co/api/v2/pokemon/1"
+
+const infoBtn = document.getElementById("info-btn");
+const movesBtn = document.getElementById("moves-btn");
+
+infoBtn.addEventListener("click", () => {
+  infoBtn.classList.add("active");
+  movesBtn.classList.remove("active");
+});
+
+movesBtn.addEventListener("click", () => {
+  movesBtn.classList.add("active");
+  infoBtn.classList.remove("active");
+});
