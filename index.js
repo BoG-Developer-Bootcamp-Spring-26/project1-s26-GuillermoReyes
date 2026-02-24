10d4a2bfa00763519c9172ff23ff2ca5adbf3444
Guillermo-Reyes1 <reyesguillermo2305@gmail.com>
2026-02-24 11:51:46 -0500

    organized functions and create variables for pokemon values

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-fetch = "https://pokeapi.co/api/v2/pokemon/1"
+const APIbase = "https://pokeapi.co/api/v2/pokemon/"
+
+let pokeID = 1;
+let mode = "info"
 
 const infoBtn = document.getElementById("info-btn");
 const movesBtn = document.getElementById("moves-btn");
+const rightBtn = document.getElementById("right-btn");
+const leftBtn = document.getElementById("left-btn");
+
+const imgElement = document.getElementById("pokemon-img");
+const nameElement = document.getElementById("pokemon-name");
+const typesElement = document.getElementById("types");
+const panelTitleElement = document.getElementById("panel-title");
+const panelBody = document.getElementById("panel-body");
 
 infoBtn.addEventListener("click", () => {
   infoBtn.classList.add("active");
   movesBtn.classList.remove("active");
+  updatePokemon()
 });
 
 movesBtn.addEventListener("click", () => {
   movesBtn.classList.add("active");
   infoBtn.classList.remove("active");
+  updatePokemon()
 });
 
-let pokeID = 1;
-const rightBtn = document.getElementById("right-btn");
-const leftBtn = document.getElementById("left-btn");
+rightBtn.addEventListener("click", () => changePokemon(+1))
+
+leftBtn.addEventListener("click", () => changePokemon(-1))
 
 function updatePokemon() {
     console.log("Current Pokemon ID: ", pokeID)
@@ -30,6 +43,3 @@ function changePokemon(val) {
     updatePokemon()
 }
 
-rightBtn.addEventListener("click", () => changePokemon(+1))
-
-leftBtn.addEventListener("click", () => changePokemon(-1))
